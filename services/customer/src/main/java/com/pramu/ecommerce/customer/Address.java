c1be26fd0b353759e259378836751ca1e7ec184e
Pramuditha Gunaratne <pramudithagn@gmail.com>
2024-08-25 19:01:59 +0530

    customer service

---
+++ services/customer/src/main/java/com/pramu/ecommerce/customer/Address.java
@@ -0,0 +1,21 @@
+package com.pramu.ecommerce.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Validated
+public class Address {
+
+    private String street;
+    private String houseNumber;
+    private String zipCode;
+}
---
+++ services/customer/src/main/java/com/pramu/ecommerce/customer/CustomerResponse.java
@@ -0,0 +1,11 @@
+package com.pramu.ecommerce.customer;
+
+public record CustomerResponse(
+        String id,
+        String firstname,
+        String lastname,
+        String email,
+        Address address
+) {
+
+}
